914c84e58cac3c97c860f45d1c0b743dc731e250
jesperwj <100865076+jesperwj@users.noreply.github.com>
2024-02-18 17:53:02 +0100

    Create different_rotations.py

---
+++ manim_examples/different_rotations/different_rotations.py
@@ -0,0 +1,12 @@
+from manim import *
+
+# manim -pqh different_rotations.py DifferentRotations
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
