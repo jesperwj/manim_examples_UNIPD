870ea40095f274dc6722bcb9f21d852ab3bb417f
jesperwj <100865076+jesperwj@users.noreply.github.com>
2024-02-18 17:56:13 +0100

    Create moving_frame_box.py

---
+++ manim_examples/moving_frame_box/moving_frame_box.py
@@ -0,0 +1,21 @@
+from manim import *
+
+# manim -pqh moving_frame_box.py MovingFrameBox
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=","f(x)\\frac{d}{dx}g(x)","+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
